de19860c0dcf7a2898c4306d4681c7851a5f564f
Piyush Raj Mishra <piyushxcoder@gmail.com>
2026-04-13 01:01:30 +0530

    refactor: export all important views and components

--- packages/kailendar/src/Kailendar/index.ts
+++ packages/kailendar/src/Kailendar/index.ts
@@ -1,3 +1,9 @@
-export { default } from "./index.tsx";
-export type { KailendarConfig } from "../utils/types";
-export type { KailendarConfig as KailendarProps } from "../utils/types";
+export { default as Kailendar } from "./index.tsx";
+export { default as MonthView } from "./MonthView";
+export { default as WeekView } from "./WeekView";
+export { default as DayView } from "./DayView";
+export { default as YearView } from "./YearView";
+export { default as MiniMonthView } from "./MiniMonthView";
+export { default as CommonHeader } from "../components/CommonHeader";
+export type * from "../utils/types";
+export * from "../utils/constants";
--- packages/kailendar/src/index.ts
+++ packages/kailendar/src/index.ts
@@ -1,1 +1,9 @@
-export { default as Kailendar, type KailendarConfig as KailendarProps } from "./Kailendar/";
+export { Kailendar } from "./Kailendar";
+export { default as MonthView } from "./Kailendar/MonthView";
+export { default as WeekView } from "./Kailendar/WeekView";
+export { default as DayView } from "./Kailendar/DayView";
+export { default as YearView } from "./Kailendar/YearView";
+export { default as MiniMonthView } from "./Kailendar/MiniMonthView";
+export { default as CommonHeader } from "./components/CommonHeader";
+export type * from "./utils/types";
+export * from "./utils/constants";
