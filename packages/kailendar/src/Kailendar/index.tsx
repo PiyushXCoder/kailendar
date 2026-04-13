52ff70b443b389987ce93d56b2e1e9d05aadc935
Piyush Raj Mishra <piyushxcoder@gmail.com>
2026-04-14 00:23:15 +0530

    fix: scroll height in day view

--- packages/kailendar/src/Kailendar/index.tsx
+++ packages/kailendar/src/Kailendar/index.tsx
@@ -33,62 +33,64 @@ export default function Kailendar({
         />
       )}
 
-      {view === KAILENDAR_VIEWS.MONTH && (
-        <MonthView
-          currentDate={currentDate}
-          getEvents={getEvents}
-          ghostEvent={ghostEvent}
-          onEventClick={onEventClick}
-          onDayClick={onDayClick}
-        />
-      )}
+      <div className={styles.content}>
+        {view === KAILENDAR_VIEWS.MONTH && (
+          <MonthView
+            currentDate={currentDate}
+            getEvents={getEvents}
+            ghostEvent={ghostEvent}
+            onEventClick={onEventClick}
+            onDayClick={onDayClick}
+          />
+        )}
 
-      {view === KAILENDAR_VIEWS.WEEK && (
-        <WeekView
-          currentDate={currentDate}
-          setCurrentDate={setCurrentDate}
-          getEvents={getEvents}
-          ghostEvent={ghostEvent}
-          showHeader={false}
-          onEventClick={onEventClick}
-          onTimeClick={onTimeClick}
-        />
-      )}
+        {view === KAILENDAR_VIEWS.WEEK && (
+          <WeekView
+            currentDate={currentDate}
+            setCurrentDate={setCurrentDate}
+            getEvents={getEvents}
+            ghostEvent={ghostEvent}
+            showHeader={false}
+            onEventClick={onEventClick}
+            onTimeClick={onTimeClick}
+          />
+        )}
 
-      {view === KAILENDAR_VIEWS.DAY && (
-        <DayView
-          currentDate={currentDate}
-          setCurrentDate={setCurrentDate}
-          getEvents={getEvents}
-          ghostEvent={ghostEvent}
-          showHeader={false}
-          onTimeClick={onTimeClick}
-          onEventClick={onEventClick}
-        />
-      )}
+        {view === KAILENDAR_VIEWS.DAY && (
+          <DayView
+            currentDate={currentDate}
+            setCurrentDate={setCurrentDate}
+            getEvents={getEvents}
+            ghostEvent={ghostEvent}
+            showHeader={false}
+            onTimeClick={onTimeClick}
+            onEventClick={onEventClick}
+          />
+        )}
 
-      {view === KAILENDAR_VIEWS.YEAR && (
-        <YearView
-          currentDate={currentDate}
-          onDayClick={onDayClick}
-          getEvents={getEvents}
-          ghostEvent={ghostEvent}
-          selectedDate={currentDate}
-          onSelectDate={setCurrentDate}
-        />
-      )}
+        {view === KAILENDAR_VIEWS.YEAR && (
+          <YearView
+            currentDate={currentDate}
+            onDayClick={onDayClick}
+            getEvents={getEvents}
+            ghostEvent={ghostEvent}
+            selectedDate={currentDate}
+            onSelectDate={setCurrentDate}
+          />
+        )}
 
-      {view === KAILENDAR_VIEWS.MONTH_MINI && (
-        <MiniMonthView
-          currentDate={currentDate}
-          setCurrentDate={setCurrentDate}
-          onDayClick={onDayClick}
-          getEvents={getEvents}
-          ghostEvent={ghostEvent}
-          selectedDate={currentDate}
-          onSelectDate={setCurrentDate}
-        />
-      )}
+        {view === KAILENDAR_VIEWS.MONTH_MINI && (
+          <MiniMonthView
+            currentDate={currentDate}
+            setCurrentDate={setCurrentDate}
+            onDayClick={onDayClick}
+            getEvents={getEvents}
+            ghostEvent={ghostEvent}
+            selectedDate={currentDate}
+            onSelectDate={setCurrentDate}
+          />
+        )}
+      </div>
     </div>
   );
 }
